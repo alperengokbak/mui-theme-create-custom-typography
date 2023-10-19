92aa42d5f2e3b79d241c15627ae915ae688fe281
Alperen Gökbak <swalperen3008@gmail.com>
2023-10-19 16:57:40 +0300

    Update theme.js

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -2,6 +2,7 @@ import { createTheme } from "@mui/material/styles";
 
 export const theme = createTheme({
   typography: {
+    // You can adjust it as you like from this section.
     subtitle3: {
       fontSize: "4rem",
     },
